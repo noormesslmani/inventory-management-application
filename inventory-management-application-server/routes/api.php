988b6eff49a9416ea31d9a980b7bd5a517dc52e3
noormesslmani <noor.messlmani@gmail.com>
2023-02-23 05:46:27 +0200

    Backend: adding routes to api.php file

--- inventory-management-application-server/routes/api.php
+++ inventory-management-application-server/routes/api.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1.0.0'], function () {
+    Route::group(['prefix' => 'auth'], function ($router) {
+        Route::post('/register', [AuthController::class, 'register']); 
+        Route::post('/login', [AuthController::class, 'login']); 
+    }); 
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('/products', [ProductController::class, 'getProductsByOwner']);
+        Route::post('/product', [ProductController::class, 'addNewProduct']);
+        Route::patch('/product/{id}', [ProductController::class, 'updateProduct']);
+        Route::delete('/product/{id}', [ProductController::class, 'deleteProduct']);
+
+        Route::get('/items/{product_id}', [ItemController::class, 'getItemsByProduct']);
+        Route::post('/items', [ItemController::class, 'addNewItems']);
+        Route::delete('/item/{id}', [ItemController::class, 'deleteItem']);
+       
+    });
+
 });
