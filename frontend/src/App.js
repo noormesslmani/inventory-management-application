97898c195f6de878dfe5e5848db0638c986717b1
noormesslmani <noor.messlmani@gmail.com>
2023-02-23 22:05:32 +0200

    Frontend: addinf routes in App root file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Landing from './views/landing';
+import { Routes, Route, BrowserRouter, Outlet, redirect } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import Products from './views/products';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
-    <div className="App">
+    <>
+       <ToastContainer />
       
-    </div>
+      <BrowserRouter>
+      <Routes>
+        
+        <Route path='/' element={<Landing/>}></Route>
+        <Route path='/products' element={<Products/>}></Route>
+      
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
