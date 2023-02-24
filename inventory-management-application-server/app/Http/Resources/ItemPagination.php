dcd970e24573161110cfda92f5ad3e9829cdc284
noormesslmani <noor.messlmani@gmail.com>
2023-02-24 02:53:50 +0200

    Backend: creating ItemPagination and ProductPagination Resources

---
+++ inventory-management-application-server/app/Http/Resources/ItemPagination.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+use App\Http\Resources\Item ;
+use App\Http\Resources\ItemCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ItemPagination extends JsonResource
+{
+    
+    public function toArray($request)
+    {
+        return [
+            'total_pages'=>$this->lastPage(),
+            'current_page'=>$this->currentPage(),
+            'items'=>new ItemCollection ($this->items())
+        ];
+    }
+}
+
