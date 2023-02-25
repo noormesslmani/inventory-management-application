0852a64832bc407dd4d1f863ed2675af6d35051b
noormesslmani <noor.messlmani@gmail.com>
2023-02-25 17:09:12 +0200

    Backend: fixing ItemPagination resource and removing ItemCollection Resource

--- inventory-management-application-server/app/Http/Resources/ItemPagination.php
+++ inventory-management-application-server/app/Http/Resources/ItemPagination.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 use App\Http\Resources\Item ;
-use App\Http\Resources\ItemCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ItemPagination extends JsonResource
@@ -13,7 +12,7 @@ class ItemPagination extends JsonResource
         return [
             'total_pages'=>$this->lastPage(),
             'current_page'=>$this->currentPage(),
-            'items'=>$this->items()?new ItemCollection ($this->items()):[]
+            'items'=>$this->items()??[]
         ];
     }
 }
