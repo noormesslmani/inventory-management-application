fd9235307d1bfc56bec8be9aa90aaec913dc59c0
noormesslmani <noor.messlmani@gmail.com>
2023-02-26 05:03:58 +0200

    Frontend: creating a profile ImagePicker component and reming product ImagePicker

---
+++ frontend/src/components/imagePicker/profileImagePicker.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+const ImagePicker = ({setBase64, user}) => {
+  const [file, setFile] = useState(null);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+    setFile(
+        Object.assign(acceptedFiles[0], {
+        preview: URL.createObjectURL(acceptedFiles[0])
+        })
+    );
+    const reader = new FileReader();
+    reader.readAsDataURL(acceptedFiles[0]);
+    reader.onload = () => {
+         setBase64(reader.result)
+    };
+    }
+});
+
+  return (
+    <div {...getRootProps()} className='self-center' >
+        <input {...getInputProps()} />
+        {file ? (
+            <img src={file.preview} alt="preview" className="h-64 w-56" />
+        ) : (
+            <img src={`http://localhost:8000/images/${user?.profile_picture}`} alt="default" className="h-64 w-56" />
+        )}
+    </div>
+  );
+};
+
+export default ImagePicker;
