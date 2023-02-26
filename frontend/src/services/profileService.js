ace21f4bcd63db03532d5a9ccfe290c06bf3c8a6
noormesslmani <noor.messlmani@gmail.com>
2023-02-26 05:04:58 +0200

    Frontend: adding profile api's and services

---
+++ frontend/src/services/profileService.js
@@ -0,0 +1,24 @@
+import { editProfile, changePassowrd } from "../api/profile";
+import { toast } from 'react-toastify';
+export const handleSaveChanges=async (setIsloading, user,base64,setUser, setIsReadOnly)=>{
+    setIsloading(true);
+    console.log('hi');
+        try{
+            const res=await editProfile({
+                ...(user.first_name!== JSON.parse(localStorage.getItem('user')).first_name  && { first_name: user.first_name}),
+                ...(user.last_name!== JSON.parse(localStorage.getItem('user')).last_name  && { last_name: user.last_name}),
+                ...(base64 && { base64_image: base64}),
+            });
+            setUser(res.data);
+            localStorage.setItem('user', JSON.stringify(res.data));
+            toast.success('Profile successfully updated');
+        }
+        catch (error){
+
+            toast.error(error.response.data.message);
+        }
+        finally{
+            setIsloading(false);
+            setIsReadOnly(true);
+        }
+}
